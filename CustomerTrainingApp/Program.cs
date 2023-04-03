974eb82967367c46d7464cd578473a32c93a7f4f
Jitahidi <morelporter.com>
2023-04-03 11:55:49 -0400

    Added connection string for heroku postgresql database

--- CustomerTrainingApp/Program.cs
+++ CustomerTrainingApp/Program.cs
@@ -6,8 +6,11 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
-builder.Services.AddDbContext<CustomerTrainingDataContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("TrainingDB"))
-);
+var connectionString = Environment.GetEnvironmentVariable("postgres:npbboblbkypzic:8f3b4b20fe05dbf0c37e771638e16f7e65d011839de7de1992aec584b32af8c3@ec2-52-1-92-133.compute-1.amazonaws.com:5432/d6vvbnu0980brq") ?? builder.Configuration.GetConnectionString("TrainingDB");
+builder.Services.AddDbContext<CustomerTrainingDataContext>(o => o.UseNpgsql(connectionString));
+
+
+//builder.Services.AddDbContext<CustomerTrainingDataContext>(o => o.UseNpgsql(builder.Configuration.GetConnectionString("TrainingDB"))
 
 //Allows any domain to be able to send http requests. Change in the future to only allow localhost:3000 to guard against security vulnerabilities. 
 builder.Services.AddCors(p => p.AddPolicy("corspolicy", build =>
